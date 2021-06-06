84fb5a8867f9efc188036e4089be8c21b10bebcc
eweagraff <weagraff.emily@gmail.com>
2021-06-05 18:34:05 -0700

    added test for engineer class

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,5 +1,14 @@
 const Engineer = require("../lib/engineer");
 
 describe("Engineer", () => {
-  
-})
+  it("it will return github when called", () => {
+    const testGitHub = "eweagraff";
+    const obj = new Engineer("emily", 1, "test@test.com", testGitHub);
+    expect(obj.getGitHub()).toEqual(testGitHub);
+  });
+  it("it will return employee role when called", () => {
+    const testRole = "Engineer";
+    const obj = new Engineer("emily", 1, "test@test.com", testRole);
+    expect(obj.getRole()).toEqual(testRole);
+  });
+});
