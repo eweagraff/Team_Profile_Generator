6b7af1d43a02db9a856fdd10ac12a808c32a0632
eweagraff <weagraff.emily@gmail.com>
2021-06-05 18:50:54 -0700

    added npm test for manager class

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,7 +1,14 @@
 const Manager = require("../lib/manager");
 
-describe("Manager", ()=> {
-  
-})
-
-
+describe("Manager", () => {
+  it("it will return an office number when called", () => {
+    const testNumber = 123;
+    const obj = new Manager("emily", 1, "test@test.com", testNumber);
+    expect(obj.getofficeNumber()).toEqual(testNumber);
+  });
+  it("it will return employee role when called", () => {
+    const testRole = "Manager";
+    const obj = new Manager("emily", 1, "test@test.com", testRole);
+    expect(obj.getRole()).toEqual(testRole);
+  });
+});
