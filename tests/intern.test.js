996bfbaaae44172911e4d88def4ef7ab3519b2da
eweagraff <weagraff.emily@gmail.com>
2021-06-05 18:44:16 -0700

    added npm test for intern class

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,5 +1,14 @@
 const Intern = require("../lib/intern");
 
 describe("Intern", () => {
-  
-})
+  it("it will return school name when called", () => {
+    const testSchool = "SDSU";
+    const obj = new Intern("emily", 1, "test@test.com", testSchool);
+    expect(obj.getSchool()).toEqual(testSchool);
+  });
+  it("it will return employee role when called", () => {
+    const testRole = "Intern";
+    const obj = new Intern("emily", 1, "test@test.com", testRole);
+    expect(obj.getRole()).toEqual(testRole);
+  });
+});
